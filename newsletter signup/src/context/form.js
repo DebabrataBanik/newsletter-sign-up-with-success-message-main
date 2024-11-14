11212e5a4493f4a756f77f2350bffb725c404447
DebabrataBanik <debabratabanik02@gmail.com>
2024-11-15 01:09:35 +0530

    Update form.js

--- newsletter signup/src/context/form.js
+++ newsletter signup/src/context/form.js
@@ -1,6 +1,8 @@
 import { useContext, createContext } from "react";
 
 export const FormContext = createContext()
+// setting anything inside the context is set as default. 
+// inside your provider all value mentioned is what matters and gets passed down to the children
 
 export const FormProvider = FormContext.Provider
 
